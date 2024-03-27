a6c77ba75398e8bb5edc430435eb31a9fe84caf6
Vyacheslav <guzxaroh@gmail.com>
2024-03-27 20:41:44 +0700

    created authManager for auth control

---
+++ EffectiveMobile_TestTask/AuthManager/AuthManager.swift
@@ -0,0 +1,15 @@
+//
+//  AuthManager.swift
+//  EffectiveMobile_TestTask
+//
+//  Created by Vyacheslav on 27.03.2024.
+//
+
+import Foundation
+
+class AuthManager {
+    
+    static let shared = AuthManager()
+    
+    var isLoggedIn: Bool?
+}
