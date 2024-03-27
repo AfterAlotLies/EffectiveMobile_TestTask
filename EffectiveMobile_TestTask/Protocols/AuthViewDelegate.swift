7e42edf4e08c464ec7cc987dec56bdf54b1f6c93
Vyacheslav <guzxaroh@gmail.com>
2024-03-27 20:42:30 +0700

    created AuthViewDelegate

---
+++ EffectiveMobile_TestTask/Protocols/AuthViewDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AuthViewDelegate.swift
+//  EffectiveMobile_TestTask
+//
+//  Created by Vyacheslav on 27.03.2024.
+//
+
+import Foundation
+
+protocol AuthViewDelegate: AnyObject {
+    func buttonActivate()
+    func buttonDeactivate()
+}
